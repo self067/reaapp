52018de4030935ccff5e841dc6eda562a3159d42
seltor <seltor@mail.ru>
2019-03-09 20:31:55 +0300

    20190309

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Grid from 'react-bootstrap/lib/Grid';
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+import styled from 'styled-components'
+
 class App extends Component {
   render() {
     return (
